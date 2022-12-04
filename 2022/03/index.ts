af058b07dac3053c1dfec5ddc9ca55d316589687
Andy Peterson <andy@ndpsoftware.com>
2022-12-03 21:51:01 -0800

    minor cleanup

--- 2022/03/index.ts
+++ 2022/03/index.ts
@@ -2,58 +2,55 @@ import * as fs from 'fs'
 
 const Lower = 'abcdefghijklmnopqrstuvwxyz'
 const Upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 type Letter = string
-type StringTuple = [string,string,string]
+type StringTuple = [string, string, string]
 
-const lines = fs.readFileSync('./sacks.txt').toString().split('\n')
+const lines = fs
+  .readFileSync('./sacks.txt')
+  .toString()
+  .split('\n')
+  .filter(s => !!s)
 
 const part1 = lines
-  .map(line => valueOf(findCommonLetters(...splitString(line))[0]))
-  .reduce((a,v) => a+v)
-console.log({part1})
+  .map(line => valueOf(commonLetters(...splitString(line))[0]))
+  .reduce((a, v) => a + v)
+console.log({ part1, expected: 7746 })
 
 const groups = lines
   .reduce((a, line) => {
     if (a[0].length < 3)
       a[0].push(line)
-
     else
       a.unshift([line])
     return a
   }, [[] as Array<String>]) as unknown as Array<StringTuple>
 
-
 const part2 = groups
-  .map(g => findCommonLetters3(...g))
+  .map(g => commonLetters(...g))
   .map(common => valueOf(common[0]))
-  .reduce((a,x) => a+x, 0)
+  .reduce((a, x) => a + x, 0)
 
-console.log({part2})
+console.log({ part2, expected: 2604 })
 
-function splitString(s: string): [string, string] {
-  const len = s.length
-  if (len % 2 !== 0) throw `not even length: ${s}`
-  return [s.slice(0, len / 2), s.slice(len/2, len)]
+function splitString (s: string): [string, string] {
+  if (s.length % 2 !== 0) throw `not even length: ${s}`
+  const half = s.length / 2
+  return [s.slice(0, half), s.slice(half)]
 }
 
 
-function findCommonLetters(strA: string, strB: string) {
-  return strA
+function commonLetters (strA: string, strB: string, strC?: string): string {
+  const common = strA
     .split('')
-    .reduce((acc,ch) => acc + (strB.indexOf(ch) < 0 ? '' : acc+ ch), '')
-}
-
-function findCommonLetters3(strA: string, strB: string, strC: string) {
-  return strA
-    .split('')
-    .reduce((acc,ch) => acc + ((strB.indexOf(ch) >= 0  && strC.indexOf(ch) >= 0)?  acc+ ch : ''), '')
+    .reduce((acc, ch) => acc + (strB.indexOf(ch) < 0 ? '' : ch), '')
+  return strC
+    ? commonLetters(common, strC)
+    : common
 }
 
 
-function valueOf(x: Letter) {
+function valueOf (x: Letter) {
   const u = Upper.indexOf(x)
   if (u >= 0) return u + 27
-
   return Lower.indexOf(x) + 1
 }
