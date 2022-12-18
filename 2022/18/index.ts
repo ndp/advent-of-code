13c38b7bf41913bb28aa910d51ca8ae172d848d4
Andy Peterson <andy@ndpsoftware.com>
2022-12-18 14:03:58 -0800

    day 18, part i

--- 2022/18/index.ts
+++ 2022/18/index.ts
@@ -1,1 +1,45 @@
+// Copyright (c) 2022
 import {readFileSync} from 'fs'
+
+const cubes =
+  readFileSync('./data.txt')
+    .toString()
+    .split('\n')
+    .filter(line => !!line)
+
+const cubesHash = cubes
+  .reduce((acc, cube) => {
+    acc[cube] = true
+    return acc
+  }, {} as Record<string, boolean>)
+
+
+const exposedSides =
+  cubes
+    .map(cube => {
+      return adjacentCubes(cube)
+        .filter( a => !cubesHash[a])
+        .length
+    })
+    .reduce((a,c) => a + c)
+
+console.log({exposedSides})
+
+
+function adjacentCubes(cube): Array<string> {
+  const cubeDims = cube.split(',').map(v => Number(v))
+
+  const transforms =[
+    [1, 0, 0],
+    [-1, 0, 0],
+    [0, 1, 0],
+    [0, -1, 0],
+    [0, 0, 1],
+    [0, 0, -1],
+  ]
+
+  return transforms
+    .map(tr =>
+      tr.map((tt, i) => String(tt + cubeDims[i]))
+        .join(','))
+}
